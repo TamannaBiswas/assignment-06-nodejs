a0d8028ccd860b093a9b3154d02ed7b124924415
Tamanna Biswas <tamannabiswas4636@gmail.com>
2026-09-08 10:27:06 +0600

    Add Home Route

--- index.js
+++ index.js
@@ -3,6 +3,18 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { "content-type": "text/html" });
+  // home page
+  if (req.url == "/") {
+    res.end(`
+      <h1>Home Page</h1>
+      <p>Welcome to my Home Page.</p>
+
+      <a href="/">Home</a>
+      <a href="/blog">Blog</a>
+      <a href="/about">About</a>
+      <a href="/contact">Contact</a>
+    `);
+  }
 });
 
 server.listen(5000, () => {
